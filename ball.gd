116c642f96753cb12d889ddad571829274627846
Dan Corder <dev@dancorder.com>
2020-06-02 17:07:39 +0100

    feat: increase ball speed on bounce

--- ball.gd
+++ ball.gd
@@ -2,12 +2,8 @@ extends KinematicBody2D
 
 var velocity = Vector2(200, 0)
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
 func _physics_process(delta):
 	var collision_info = move_and_collide(velocity * delta)
 	if collision_info:
+		velocity = velocity * 1.01
 		velocity = velocity.bounce(collision_info.normal)
