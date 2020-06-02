2f4a15717b85746a42e776e9fb55844e574c9a83
Dan Corder <dev@dancorder.com>
2020-06-02 22:52:35 +0100

    refactor: Tidy up court script

--- court.gd
+++ court.gd
@@ -3,7 +3,6 @@ extends Node2D
 const winningScore = 7
 const ballSpeed = 200
 const ballScene = preload("res://ball.tscn")
-const paddleScene = preload("res://paddle.tscn")
 
 var _leftScore = 0
 var _rightScore = 0
@@ -12,20 +11,19 @@ var _ball
 func _ready():
 	randomize()
 	
-	var leftPaddle = paddleScene.instance()
-	leftPaddle.init('ui_leftup', 'ui_leftdown')
-	leftPaddle.position = Vector2(80, 310)
-	add_child(leftPaddle)
-	
-	var rightPaddle = paddleScene.instance()
-	rightPaddle.init('ui_rightup', 'ui_rightdown')
-	rightPaddle.position = Vector2(944, 310)
-	add_child(rightPaddle)
-	
-	serve_ball()
+	$leftPaddle.init('ui_leftup', 'ui_leftdown')
+	$rightPaddle.init('ui_rightup', 'ui_rightdown')
+
+	new_point()
 
 
-func serve_ball():
+func new_point():
+	$rightScoreLabel.text = String(_rightScore)
+	$leftScoreLabel.text = String(_leftScore)
+	
+	if (_leftScore >= winningScore || _rightScore >= winningScore):
+		get_tree().change_scene('res://winScreen.tscn')
+	
 	var ball = ballScene.instance()
 	var vx = rand_range(-1, 1)
 	var vy = rand_range(-0.3, 0.3)
@@ -36,22 +34,13 @@ func serve_ball():
 	_ball = ball
 
 
-func new_point():
-	_ball.queue_free()
-	$rightScoreLabel.text = String(_rightScore)
-	$leftScoreLabel.text = String(_leftScore)
-	
-	if (_leftScore >= winningScore || _rightScore >= winningScore):
-		get_tree().change_scene('res://winScreen.tscn')
-
-	serve_ball()
-
-
 func _on_leftWall_side_wall_hit():
+	_ball.queue_free()
 	_rightScore += 1
 	new_point()
 	
 
 func _on_rightWall_side_wall_hit():
+	_ball.queue_free()
 	_leftScore += 1
 	new_point()
