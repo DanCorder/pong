c40f53a270e60d49df8012e7293b96bb77630ee3
Dan Corder <dev@dancorder.com>
2020-06-02 19:55:25 +0100

    refactor: remove debug prints

--- court.gd
+++ court.gd
@@ -30,9 +30,7 @@ func _ready():
 func _on_leftWall_side_wall_hit():
 	rightScore += 1
 	$rightScoreLabel.text = String(rightScore)
-	print('Score: ' + String(leftScore) + ':' + String(rightScore))
 
 func _on_rightWall_side_wall_hit():
 	leftScore += 1
 	$leftScoreLabel.text = String(leftScore)
-	print('Score: ' + String(leftScore) + ':' + String(rightScore))
