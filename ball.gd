854f087bc0e395abb4f7ee258c40f42badf4cdfc
Dan Corder <dev@dancorder.com>
2020-06-02 16:34:16 +0100

    feat: ball bounces off paddles and walls

--- ball.gd
+++ ball.gd
@@ -1,10 +1,6 @@
 extends KinematicBody2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+var velocity = Vector2(200, 0)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,8 +8,6 @@ func _ready():
 
 
 func _physics_process(delta):
-	move_and_collide(Vector2(1, 1)) # Move right 1 pixel per physics frame
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	var collision_info = move_and_collide(velocity * delta)
+	if collision_info:
+		velocity = velocity.bounce(collision_info.normal)
