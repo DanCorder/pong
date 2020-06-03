530775ba5bae960fa18cb64cb4ca50fe40d03a1c
Dan Corder <dev@dancorder.com>
2020-06-03 22:07:15 +0100

    feat: add different bounce angles on paddles

--- ball.gd
+++ ball.gd
@@ -1,5 +1,7 @@
 extends KinematicBody2D
 
+const speed_up = 1.03
+const paddleHeight = 80
 var velocity = Vector2(200, 0)
 
 func init(initialVelocity):
@@ -8,5 +10,12 @@ func init(initialVelocity):
 func _physics_process(delta):
 	var collision_info = move_and_collide(velocity * delta)
 	if collision_info:
-		velocity = velocity * 1.03
-		velocity = velocity.bounce(collision_info.normal)
+		if collision_info.collider.has_method('custom_bounce_direction'):
+			var current_speed = velocity.length()
+			var new_direction = collision_info.collider.custom_bounce_direction(collision_info)
+			velocity = new_direction * current_speed
+		else:
+			velocity = velocity.bounce(collision_info.normal)
+		
+		velocity = velocity * speed_up
+		
--- paddle.gd
+++ paddle.gd
@@ -1,5 +1,7 @@
 extends KinematicBody2D
 
+const paddle_height = 80
+
 var speed = 300
 var velocity = Vector2()
 var upKey = 'ui_up'
@@ -17,6 +19,13 @@ func get_input():
 	if Input.is_action_pressed(upKey):
 		velocity.y -= 1
 	velocity = velocity.normalized() * speed
+
+# Allow the player to aim shots by adjusting bounce angle based on where
+# on the paddle the ball bounces
+func custom_bounce_direction(collision_info):
+	var newVx = 1 if collision_info.normal.x > 0 else -1
+	var newVy = (collision_info.position.y - collision_info.collider.position.y) / paddle_height
+	return Vector2(newVx, newVy).normalized()
 
 func _physics_process(delta):
 	get_input()
