d33f77033b307b2edb9123f9ca9620282d0ed0c6
Dan Corder <dev@dancorder.com>
2020-06-05 21:38:51 +0100

    feat: add court selection

--- title.gd
+++ title.gd
@@ -1,5 +1,10 @@
 extends Control
 
-func _process(delta: float) -> void:
-	if Input.is_action_pressed('ui_accept'):
-		get_tree().change_scene('res://courts/base.tscn')
+func _input(_ev) -> void:
+	var _dummy: int;
+	if Input.is_key_pressed(KEY_1):
+		_dummy = get_tree().change_scene('res://courts/base.tscn')
+	if Input.is_key_pressed(KEY_2):
+		_dummy = get_tree().change_scene('res://courts/compass/court.tscn')
+	if Input.is_key_pressed(KEY_3):
+		_dummy = get_tree().change_scene("res://courts/pachinko/court.tscn")
--- winScreen.gd
+++ winScreen.gd
@@ -5,4 +5,4 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 	if Input.is_action_pressed('ui_accept'):
-		get_tree().change_scene('res://courts/base.tscn')
+		get_tree().change_scene('res://title.tscn')
