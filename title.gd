c2ff418a08899b9cdeac77168e7c3e6db8101417
Dan Corder <dev@dancorder.com>
2020-06-05 20:11:10 +0100

    feat: add compass court

--- title.gd
+++ title.gd
@@ -2,4 +2,4 @@ extends Control
 
 func _process(delta: float) -> void:
 	if Input.is_action_pressed('ui_accept'):
-		get_tree().change_scene('res://court.tscn')
+		get_tree().change_scene('res://courts/base.tscn')
