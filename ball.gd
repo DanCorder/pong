241ccd77d0b04197fd057125cabbe0a44bc86859
Dan Corder <dev@dancorder.com>
2020-06-05 22:22:49 +0100

    refactor: rename member variables

--- ball.gd
+++ ball.gd
@@ -1,15 +1,15 @@
 extends KinematicBody2D
 
-var velocity = Vector2(200, 0)
+var _velocity = Vector2(200, 0)
 
 func init(initialVelocity: Vector2) -> void:
-	velocity = initialVelocity
+	_velocity = initialVelocity
 
 func _physics_process(delta: float) -> void:
-	var collision_info = move_and_collide(velocity * delta)
+	var collision_info = move_and_collide(_velocity * delta)
 	if collision_info:
 		if collision_info.collider.has_method('custom_bounce'):
-			var current_speed = velocity.length()
-			velocity = collision_info.collider.custom_bounce(collision_info, current_speed)
+			var current_speed = _velocity.length()
+			_velocity = collision_info.collider.custom_bounce(collision_info, current_speed)
 		else:
-			velocity = velocity.bounce(collision_info.normal)
+			_velocity = _velocity.bounce(collision_info.normal)
--- paddle.gd
+++ paddle.gd
@@ -4,25 +4,29 @@ const paddle_height = 80
 const bounce_speed_up = 1.05
 const speed = 600
 
-var upKey = 'ui_up'
-var downKey = 'ui_down'
+var _upKey = 'ui_up'
+var _downKey = 'ui_down'
+
 
 func init(up: String, down: String) -> void:
-	upKey = up
-	downKey = down
+	_upKey = up
+	_downKey = down
 
-func get_velocity_from_input() -> Vector2:
-	# Detect up/down keystate and only move when pressed
-	var velocity = Vector2()
-	if Input.is_action_pressed(downKey):
-		velocity.y += 1
-	if Input.is_action_pressed(upKey):
-		velocity.y -= 1
-	return velocity.normalized() * speed
 
 func _physics_process(delta: float):
 	var velocity = get_velocity_from_input()
 	move_and_collide(velocity * delta)
+
+
+func get_velocity_from_input() -> Vector2:
+	# Detect up/down keystate and only move when pressed
+	var velocity = Vector2()
+	if Input.is_action_pressed(_downKey):
+		velocity.y += 1
+	if Input.is_action_pressed(_upKey):
+		velocity.y -= 1
+	return velocity.normalized() * speed
+
 
 # Allow the player to aim shots by adjusting bounce angle based on where
 # on the paddle the ball bounces
